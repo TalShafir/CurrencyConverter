217f79de1e6eac6e1fa5e39fd771ef3d531b5c2e
Tal Shafir <tal.4000@gmail.com>
2020-11-09 18:15:38 +0200

    added tests to check that the we can access the api endpoint and that our service calculate the new value correctly

--- tests/test_currency_converter_service.py
+++ tests/test_currency_converter_service.py
@@ -1,9 +1,18 @@
 import unittest
 
+from currency_converter_service import CurrencyConverterServiceFactory
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+class TestCurrencyConverterService(unittest.TestCase):
+    service = CurrencyConverterServiceFactory.from_config_file('currency_converter.config')
+
+    def test_sanity(self):
+        self.assertEqual(self.service.convert('USD', 'USD', 1), 1)
+
+    def test_service(self):
+        conversion_ratio = self.service.convert('USD', 'ILS', 1)
+        value = 50
+        self.assertEqual(value * conversion_ratio, self.service.convert('USD', 'ILS', value))
 
 
 if __name__ == '__main__':
