5ef4fe94568c9bf5a30b2294384dcb96d1bca3d2
Tal Shafir <tal.4000@gmail.com>
2020-11-10 17:05:18 +0200

    using the dependency_injector package

--- currency_converter.py
+++ currency_converter.py
@@ -1,8 +1,12 @@
 import argparse
 import sys
+from typing import List
+
+from dependency_injector import containers, providers
+from dependency_injector.wiring import Provide
 
 from input_file_parser import InputFileParser
-from currency_converter_service import CurrencyConverterServiceFactory
+from currency_converter_service import CurrencyConverterService
 
 _INPUT_FILE_HELP = "file that contains source and target currencies and a list of values, all separated by nextline"
 
@@ -14,26 +18,52 @@ def parse_args(argv):
     :return: Namespace object containing the parsed argv
     """
     parser = argparse.ArgumentParser(description="Tool to convert currency")
-
     parser.add_argument("-f", "--input-file", type=str, help=_INPUT_FILE_HELP)
-
     return parser.parse_args(argv)
 
 
-def currency_converter(input_file, input_file_parser_service=None, converter_service=None):
+class Container(containers.DeclarativeContainer):
+    """
+    Container class to manage the dependency injection
+    """
+    config = providers.Configuration()
+
+    input_file_parser = providers.Singleton(
+        InputFileParser
+    )
+
+    converter_service = providers.Singleton(
+        CurrencyConverterService,
+        api_key=config.AUTH.api_key,
+        api_endpoint_address=config.AUTH.api_endpoint_address,
+    )
+
+
+def currency_converter(input_file: str,
+                       input_file_parser_service: InputFileParser = Provide[Container.input_file_parser],
+                       converter_service: CurrencyConverterService = Provide[Container.converter_service]) \
+        -> List[float]:
     src_currency, target_currency, values = input_file_parser_service.parse(input_file)
     converted_values = [converter_service.convert(src_currency, target_currency, value) for value in values]
     return converted_values
 
 
 def main(argv):
+    # read argv
     args = parse_args(argv)
-    input_file_parser = InputFileParser()
-    converter_service = CurrencyConverterServiceFactory.from_config_file('currency_converter.config')
 
-    print('\n'.join(currency_converter(input_file=args.input_file,
-                                       input_file_parser_service=input_file_parser,
-                                       converter_service=converter_service)))
+    # configure dependency injection
+    container = Container()
+    container.config.from_ini('currency_converter.config')
+    container.wire(modules=[sys.modules[__name__]])
+
+    # execute the conversion
+    converted_values = currency_converter(args.input_file)
+    # join the values to be one string separated by \n
+    print('\n'.join(
+        # for each value round it 2 digits after the decimal point and convert to string
+        map(lambda val: str(round(val, 2)),
+            converted_values)))
 
 
 if __name__ == '__main__':
