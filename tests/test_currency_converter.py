1ff003f4fb62285da886170af28f190c3641def4
Tal Shafir <tal.4000@gmail.com>
2020-11-10 17:17:18 +0200

    use the dependency injection package for the integration tests

--- tests/test_currency_converter.py
+++ tests/test_currency_converter.py
@@ -1,15 +1,19 @@
 import unittest
 from unittest.mock import patch, mock_open
+import sys
 
 from currency_converter_service import CurrencyConverterService
 from input_file_parser import InputFileParser
-from currency_converter import currency_converter
+from currency_converter import currency_converter, Container
 
 
 class TestCurrencyConverter(unittest.TestCase):
     """
     Tests for the whole conversion process
     """
+    # configure dependency injection
+    container = Container()
+    container.wire(modules=[sys.modules[__name__]])
 
     def test_currency_converter(self):
         """
@@ -20,11 +24,13 @@ class TestCurrencyConverter(unittest.TestCase):
                                1
                                5
                                10.5"""
+
+        # mock the input file to be read from string
         with patch("builtins.open", mock_open(read_data=_TEST_INPUT_FILE)):
-            converted_values = currency_converter('/path/to/input',
-                                                  input_file_parser_service=InputFileParser(),
-                                                  converter_service=MockCurrencyConverterService(None, None))
-            self.assertEqual(converted_values, [3.5, 17.5, 36.75])
+            # inject mock converter service
+            with self.container.converter_service.override(MockCurrencyConverterService(None, None)):
+                converted_values = currency_converter('/path/to/input')
+                self.assertEqual(converted_values, [3.5, 17.5, 36.75])
 
     def test_no_values(self):
         """
@@ -32,11 +38,12 @@ class TestCurrencyConverter(unittest.TestCase):
         """
         _TEST_INPUT_FILE = """USD
                                ILS"""
+        # mock the input file to be read from string
         with patch("builtins.open", mock_open(read_data=_TEST_INPUT_FILE)):
-            converted_values = currency_converter('/path/to/input',
-                                                  input_file_parser_service=InputFileParser(),
-                                                  converter_service=MockCurrencyConverterService(None, None))
-            self.assertEqual(converted_values, [])
+            # inject mock converter service
+            with self.container.converter_service.override(MockCurrencyConverterService(None, None)):
+                converted_values = currency_converter('/path/to/input')
+                self.assertEqual(converted_values, [])
 
 
 class MockCurrencyConverterService(CurrencyConverterService):
